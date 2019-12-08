03c35e95eca2609777361d05539f059ed82e7cbc
Pratyush Debnath <pratyush.debnath93@gmail.com>
2019-12-08 08:08:20 +0530

    Add files via upload

--- flask_new.py
+++ flask_new.py
@@ -4,14 +4,14 @@ from keras.preprocessing.image import img_to_array,load_img
 from keras.models import load_model
 from keras import backend as K
 import numpy as np
-import argparse
-import imutils
+import random
 import cv2
+from keras import backend as K
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('test.html')
+    return render_template('test1.html')
 
 @app.route('/submit', methods = ['GET', 'POST'])
 def upload_file():
@@ -29,22 +29,38 @@ def predict(filename):
 	orig = image1.copy()
 	# pre-process the image for classification
 	#image = cv2.resize(image, (28, 28))
-	image = load_img(filename,target_size=(28,28))
+	image = load_img(filename,target_size=(150,150))
 	image = img_to_array(image)
 	image = np.expand_dims(image, axis=0)
 	#K.clear_session()
 	# load the trained convolutional neural network
 	print("[INFO] loading network...")
-	model = load_model('model_new.h5')
+	model = load_model('model_project6.h5')
+	model.load_weights('weights_project6.h5')
 	# classify the input image
-	(normal, pnemonia) = model.predict(image)[0]
-	# build the label
-	label = "NORMAL" if normal > pnemonia else "PNEMONIA"
-	proba = normal if normal > pnemonia else pnemonia
+	print("-"*100)
+	result  = model.predict(image)
+	results = np.array(result, dtype=float)
+	pred = result[0][np.argmax(result)] * 100
+	answer = np.argmax(results)
+	print(pred)
+	pred1 = "pnemonia"
+	pred2 = "not pnemonia"
+	if answer == 0:
+		return render_template('test1.html',results=pred1)
+	if answer == 1:
+		return render_template('test1.html',results=pred2)
+	#(normal, pnemonia) = model.predict(image)[0]
+	# # build the label
+	#label = "NORMAL" if normal > pnemonia else "PNEMONIA"
+	#proba = normal if normal > pnemonia else pnemonia
 	#label = "{}: {:.2f}%".format(label, proba * 100)
 	# draw the label on the image
 	#K.clear_session()
-	return label
+	#li = ['normal','pnemonia']
+	#label - random.choice(li)
+	#K.clear_session()
+	return 0
 
 if __name__ == "__main__":
     app.run(debug=True)
